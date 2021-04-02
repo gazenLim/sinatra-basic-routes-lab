5615608dbda6ab4c8b65a7ec307b5692acd63820
gajen <meetgajen@yahoo.com>
2021-04-02 18:28:53 -0400

    commit tree 7aa4629d2f6e1dc05e481e605cf54891b5022c8f

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+      end 
+
+
+      get '/name' do
+        "My name is Gajen"
+      end
+
+      get '/hometown' do
+        "My hometown is Colombus"
+      end
+
+      get '/favorite-song' do
+       " My favorite song is we are the world"
+      end
 end
